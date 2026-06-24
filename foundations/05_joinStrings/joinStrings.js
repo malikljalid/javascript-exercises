b622ec48330329efe893b2cf550334dfdab4f97a
malikljalid <181765794+malikljalid@users.noreply.github.com>
2026-06-24 01:02:22 +0100

    Add 2 new variables fullName and age and update greating value

--- foundations/05_joinStrings/joinStrings.js
+++ foundations/05_joinStrings/joinStrings.js
@@ -12,8 +12,10 @@ const birthYear = 1947;
 const thisYear = 1965;
 const firstName = "Carlos";
 const lastName = "Stevenson";
+const fullName = firstName + " " + lastName;
+const age = thisYear - birthYear
 
-const greeting = "Hello! My name is " + firstName + " " + lastName + " and I am " + (thisYear - birthYear) + " years old.";
+const greeting = "Hello! My name is " + fullName + " and I am " + age + " years old.";
 
 
 
