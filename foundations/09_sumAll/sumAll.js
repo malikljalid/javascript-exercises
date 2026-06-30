845822aae4a550c3f3ba73d3c52f09c042f15240
malikljalid <181765794+malikljalid@users.noreply.github.com>
2026-06-30 13:54:50 +0100

    Exercice 09 is DONE

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,16 @@
-const sumAll = function() {
+const sumAll = function(a, b) {
+    if (!Number.isInteger(a) || !Number.isInteger(b) || a < 0 || b < 0)
+        return ("ERROR");
 
+    let sum = 0;
+    let min = (a > b) ? b : a;
+    let max = (a > b) ? a : b;
+
+    for (let i = min; i <= max; i++) {
+        sum += i;
+    }
+
+    return (sum);
 };
 
 // Do not edit below this line
