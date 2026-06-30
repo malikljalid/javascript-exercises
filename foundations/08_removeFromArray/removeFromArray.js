39a3cd71a293dffc279624db289002e84588a02a
malikljalid <181765794+malikljalid@users.noreply.github.com>
2026-06-30 13:32:05 +0100

    Exercice 08 is DONE

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,11 @@
-const removeFromArray = function() {
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args)
+    {
+        while (arr.includes(arg))
+            arr.splice(arr.indexOf(arg), 1);
+    }
+
+    return (arr);
 };
 
 // Do not edit below this line
